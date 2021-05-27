4f3c23cc06cbc270edc6e0207023d7abf544b77d
Adrián <42869274+adriancanog@users.noreply.github.com>
2021-05-27 12:45:52 -0300

    modificacion modelo order

--- models/order.js
+++ models/order.js
@@ -1,73 +1,71 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const autoIncrementModelID = require('./counterModel');
-const orderSchema = Schema({
-  id: { type: Number, unique: true, min: 1 },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
- 
-  sigma: {
-    type: String,
-    required: false
-  },
-  cart: {
-    totalQty: {
-      type: Number,
-      default: 0,
-      required: false,
+const autoIncrementModelID = require("./counterModel");
+const orderSchema = Schema(
+  {
+    id: { type: Number, unique: true, min: 1 },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    totalCost: {
-      type: Number,
-      default: 0,
-      required: false,
-    },
-    items: [
-      {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-        },
-        qty: {
-          type: Number,
-          default: 0,
-        },
-        price: {
-          type: Number,
-          default: 0,
-        },
-        title: {
-          type: String,
-        },
-        code: {
-          type: String,
-        },
-      },
-    ],
-  },
-  address: {
-    type: String,
-    required: false,
-  },
-  
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  Delivered: {
-    type: Boolean,
-    default: false,
-  },
-});
 
-orderSchema.pre('save', function (next) {
+    sigma: {
+      type: String,
+      required: false,
+    },
+    cart: {
+      totalQty: {
+        type: Number,
+        default: 0,
+        required: false,
+      },
+      totalCost: {
+        type: Number,
+        default: 0,
+        required: false,
+      },
+      items: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+          },
+          qty: {
+            type: Number,
+            default: 0,
+          },
+          price: {
+            type: Number,
+            default: 0,
+          },
+          title: {
+            type: String,
+          },
+          code: {
+            type: String,
+          },
+        },
+      ],
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    Delivered: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+orderSchema.pre("save", function (next) {
   if (!this.isNew) {
     next();
     return;
   }
 
-  autoIncrementModelID('activities', this, next);
+  autoIncrementModelID("activities", this, next);
 });
 module.exports = mongoose.model("Order", orderSchema);
- 
